b09f851b2c0167a423088a96fb6bb3ad01e3a8d4
onurd <106928484+onurdzd@users.noreply.github.com>
2023-01-31 20:36:06 +0300

    sorular cevaplandı (gonderiyiBegen ve arama eklenecek)

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@
 */
 
 // State hook u import edin
-import React from 'react';
-
+import React from "react";
+import { useState } from "react";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import "./App.css";
+import sahteVeri from "./sahte-veri";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
+
+  const [gonderiler, setGönderiler] = useState([...sahteVeri]);
+  const [arama, setArama] = useState();
+
+  const gonderiyiBegen = (gonderiID) => {
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -28,11 +34,20 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
+    // gonderiler.find((elem) => elem.id === gonderiID).likes + 1
+    // gonderiler.map((item) => {
+    //   if (item.id === gonderiID) {
+    //     setGönderiler([...gonderiler, item.likes + 1]);
+    //     console.log(gonderiler);
+    //   }
+    // });
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaÇubuğu />
+      <Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} />
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
   );
